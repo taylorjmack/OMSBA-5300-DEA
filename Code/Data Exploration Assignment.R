21c4f7845989933661cf26648b1b6924ec597790
tmack22 <tmack@seattleu.edu>
2022-08-07 09:32:38 -0700

    Adding date formatting on the month column

--- Code/Data Exploration Assignment.R
+++ Code/Data Exploration Assignment.R
@@ -2,13 +2,20 @@ library(plyr)
 library(purrr)
 library(dplyr)
 library(tidyverse)
+library(lubridate)
+
 setwd('./Data/')
-trend_data <- list.files(pattern = 'trends_up_to_') %>%
-           map(read_csv) %>%
+trend_data <- list.files(pattern = 'trends_up_to_', full.names = TRUE) %>%
+           map_df(read_csv) %>%
            bind_rows()
 
 scorecard_dict <- read_csv('./CollegeScorecardDataDictionary-09-08-2015.csv')
 id_name_link <- read_csv('./id_name_link.csv')
 most_recent_cohorts <- read_csv('./Most+Recent+Cohorts+(Scorecard+Elements).csv')
 
-data <- ldply('./Data/'+f_list, read.csv)
+trend_data$monthorweek <- str_sub(trend_data$monthorweek,1,10) %>%
+                          ymd() %>%
+                          floor_date(unit= 'month')
+colnames(trend_data)[5] <- "month"
+
+
